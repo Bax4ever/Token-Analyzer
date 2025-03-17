f60c9b222e8e5eaf5ade2a78213f73e3a01c54a0
Bax4ever <bartol.mijatovic@outlook.com>
2025-03-18 00:18:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from bot.config import botbundler_token
 import logging
 import os
 
-WEBHOOK_URL = os.getenv("https://token-analyzer-production.up.railway.app/webhook")  # Set this in Railway/Render
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Set this in Railway/Render
 
 logging.basicConfig(level=logging.INFO)
 logging.info("✅ Bot is starting with Webhooks...")
